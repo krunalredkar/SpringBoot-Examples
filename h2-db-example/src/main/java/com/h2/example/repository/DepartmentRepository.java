bf5795fffea4fe00069fc848b218fa9ad2de2795
redkar.krunal <krunal.redkar@wowonii.com>
2022-03-10 16:35:54 +0530

    Implemented Department Repository interface.

---
+++ h2-db-example/src/main/java/com/h2/example/repository/DepartmentRepository.java
@@ -0,0 +1,11 @@
+package com.h2.example.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.h2.example.entity.Department;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long>{
+
+}
