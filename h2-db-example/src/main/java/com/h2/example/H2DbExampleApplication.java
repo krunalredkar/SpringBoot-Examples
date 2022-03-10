3103dac5f712192449bd5c4a377f8209177fc292
redkar.krunal <krunal.redkar@wowonii.com>
2022-03-10 16:38:40 +0530

    Main class.

---
+++ h2-db-example/src/main/java/com/h2/example/H2DbExampleApplication.java
@@ -0,0 +1,13 @@
+package com.h2.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2DbExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2DbExampleApplication.class, args);
+	}
+
+}
